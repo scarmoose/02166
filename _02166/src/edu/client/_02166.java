bf26af5df5a67370b70b6cb84b3e28ad83aed418
thomas <thomas@liljegren.dk>
2015-08-03 13:25:41 +0200

    whaddup

--- _02166/src/edu/client/_02166.java
+++ _02166/src/edu/client/_02166.java
@@ -1,6 +1,9 @@
 package edu.client;
 
 import com.google.gwt.core.client.EntryPoint;
+import com.google.gwt.user.client.ui.RootPanel;
+
+import edu.client.ui.MainView;
 
 /**
  * Entry point classes define <code>onModuleLoad()</code>.
@@ -12,6 +15,13 @@ public class _02166 implements EntryPoint {
 	 */
 	
 	public void onModuleLoad() {
-		
+		MainView main;
+		try {
+			main = new MainView();
+			RootPanel.get().add(main);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
