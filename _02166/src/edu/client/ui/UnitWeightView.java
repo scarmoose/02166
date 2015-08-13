9cb90779cc1280509fb0fb15d344602b10c7dc2c
PaulinDelvaux <poll3xd@gmail.com>
2015-08-13 12:31:19 +0200

    Bedste commit fra UnitWeight so far

--- _02166/src/edu/client/ui/UnitWeightView.java
+++ _02166/src/edu/client/ui/UnitWeightView.java
@@ -19,7 +19,11 @@ public class UnitWeightView extends Composite {
 	private VerticalPanel vPanel = new VerticalPanel();
 	private HorizontalPanel hPanel1 = new HorizontalPanel();
 	private HorizontalPanel hPanel2 = new HorizontalPanel();
+	private HorizontalPanel hPanel3 = new HorizontalPanel();
+	private HorizontalPanel hPanel4 = new HorizontalPanel();
 	private Label weightLabel = new Label("Netto");
+	private Label errorLabel1 = new Label();
+	private Label errorLabel2 = new Label();
 	private Label lblOne = new Label("Init Units");
 	private Label lblTwo = new Label("Total Units");
 	private TextBox initBox = new TextBox();
@@ -39,39 +43,10 @@ public class UnitWeightView extends Composite {
 		initWidget(this.vPanel);
 		weightLabel.addStyleName("weightLabel");
 		vPanel.add(weightLabel);
-		vPanel.add(hPanel1);
-		vPanel.add(hPanel2);
+		vPanel.add(hPanel1);vPanel.add(hPanel2);vPanel.add(hPanel3);vPanel.add(hPanel4);
 		unitText.setEnabled(false);
-
-		//-------------------------------------------
-		//Weight-button, get
-		//-------------------------------------------
-
-		/*		Button getWeightButton = new Button("Get weight", new ClickHandler(){
-			@Override
-			public void onClick(ClickEvent event){
-				try {
-					c.getASEService().getSWeight(new AsyncCallback<Double>(){
-
-						@Override
-						public void onFailure(Throwable caught) {
-							Window.alert("An error occured: " + caught.getMessage());
-						}
-
-						@Override
-						public void onSuccess(Double result) {
-							weightLabel.setText("Netto: " + result + " kg");
-						}
-					});
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
-		});
-
-		hPanel1.add(getWeightButton);
-		getWeightButton.setPixelSize( 105, 30);
-		hPanel1.setBorderWidth(3);*/
+		
+		
 		hPanel1.add(lblOne);
 		lblOne.setPixelSize(50, 40);
 		hPanel1.add(initBox);
@@ -87,14 +62,27 @@ public class UnitWeightView extends Composite {
 
 						@Override
 						public void onFailure(Throwable caught) {
-							Window.alert("An error occured: " + caught.getMessage());
+							hPanel3.clear();hPanel4.clear();
+							errorLabel1.setText("Der er fejl på vægten.");
+							errorLabel2.setText("Tjek eventuelt om maksbelastningen er overskredet");
+							hPanel3.add(errorLabel1);
+							hPanel4.add(errorLabel2);
 						}
 
 						@Override
 						public void onSuccess(Double result) {
-							double units = Math.round(result/unitW);
+						double units = Math.round(result/unitW);
+						if(units < 0){
+							hPanel3.clear();hPanel4.clear();
+							errorLabel1.setText("Der er mindre på vægten end ved kalibreringen");
+							hPanel3.add(errorLabel1);
+							
+						}
+						else{
 							weightLabel.setText("Netto: " + result + " kg");
 							unitText.setText(""+units);
+						}	
+							
 						}
 					});
 				} catch (Exception e) {
@@ -117,19 +105,35 @@ public class UnitWeightView extends Composite {
 
 						@Override
 						public void onFailure(Throwable caught) {
-							Window.alert("An error occured: " + caught.getMessage());
+							hPanel3.clear();hPanel4.clear();
+							errorLabel1.setText("Der er fejl på vægten.");
+							errorLabel2.setText("Tjek eventuelt om maksbelastningen er overskredet");
+							hPanel3.add(errorLabel1);
+							hPanel4.add(errorLabel2);
+							
 						}
 
 						@Override
 						public void onSuccess(Double result) {
-							double antal = Double.parseDouble(initBox.getText());
-							if(antal<1 || result == 0){
-								Window.alert("Din fejl virker");
-							}
-							else{
-								unitW = result/antal;
-								weightLabel.setText("Netto: " + result + " kg");
-								UnitButton.setEnabled(true);
+							double antal;
+							try {
+								antal = Integer.parseInt(initBox.getText());
+								if(antal<1 || result == 0){
+									hPanel3.clear();hPanel4.clear();
+									errorLabel1.setText("Ved kalibrering skal der være mere end 1 af produktet");
+									hPanel3.add(errorLabel1);
+									
+								}
+								else{
+									unitW = result/antal;
+									weightLabel.setText("Netto: " + result + " kg");
+									UnitButton.setEnabled(true);
+									hPanel3.clear();hPanel4.clear();
+								}
+							} catch (NumberFormatException e) {
+								hPanel3.clear();hPanel4.clear();
+								errorLabel1.setText("Antallet skal være i heltal");
+								hPanel3.add(errorLabel1);
 							}
 						}
 					});
